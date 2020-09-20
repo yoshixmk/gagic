f2ab55cc8fb861e53203b0390e6cd450db28d5d4
yoshixmk <pirori67@gmail.com>
2020-09-20 12:48:12 +0900

    update 1.4.1

--- src/plugins/i18n_script.js
+++ src/plugins/i18n_script.js
@@ -1,5 +1,5 @@
 import i18next from "https://deno.land/x/i18next@v19.6.3/index.js";
-import { Trans } from "https://deno.land/x/gagic@v0.9.0/src/vendors/react-i18next/Trans.js";
+import { Trans } from "https://deno.land/x/gagic@0.9.2/src/vendors/react-i18next/Trans.js";
 
 import gagicConfig from "./gagic.config.js";
 
