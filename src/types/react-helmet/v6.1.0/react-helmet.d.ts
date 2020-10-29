aea0f34b492f067f8189551279a43ab397d600ce
yoshixmk <pirori67@gmail.com>
2020-10-29 21:56:03 +0900

    fmt

--- src/types/react-helmet/v6.1.0/react-helmet.d.ts
+++ src/types/react-helmet/v6.1.0/react-helmet.d.ts
@@ -10,81 +10,85 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
-import { React } from '../../../../deps.ts';
+import { React } from "../../../../deps.ts";
 
 interface OtherElementAttributes {
-    [key: string]: string | number | boolean | null | undefined;
+  [key: string]: string | number | boolean | null | undefined;
 }
 
-type HtmlProps = JSX.IntrinsicElements['html'] & OtherElementAttributes;
+type HtmlProps = JSX.IntrinsicElements["html"] & OtherElementAttributes;
 
-type BodyProps = JSX.IntrinsicElements['body'] & OtherElementAttributes;
+type BodyProps = JSX.IntrinsicElements["body"] & OtherElementAttributes;
 
-type LinkProps = JSX.IntrinsicElements['link'];
+type LinkProps = JSX.IntrinsicElements["link"];
 
-type MetaProps = JSX.IntrinsicElements['meta'];
+type MetaProps = JSX.IntrinsicElements["meta"];
 
 export interface HelmetTags {
-    baseTag: Array<any>;
-    linkTags: Array<HTMLLinkElement>;
-    metaTags: Array<HTMLMetaElement>;
-    noscriptTags: Array<any>;
-    scriptTags: Array<HTMLScriptElement>;
-    styleTags: Array<HTMLStyleElement>;
+  baseTag: Array<any>;
+  linkTags: Array<HTMLLinkElement>;
+  metaTags: Array<HTMLMetaElement>;
+  noscriptTags: Array<any>;
+  scriptTags: Array<HTMLScriptElement>;
+  styleTags: Array<HTMLStyleElement>;
 }
 
 export interface HelmetProps {
-    async?: boolean;
-    base?: any;
-    bodyAttributes?: BodyProps;
-    defaultTitle?: string;
-    defer?: boolean;
-    encodeSpecialCharacters?: boolean;
-    htmlAttributes?: HtmlProps;
-    onChangeClientState?: (newState: any, addedTags: HelmetTags, removedTags: HelmetTags) => void;
-    link?: LinkProps[];
-    meta?: MetaProps[];
-    noscript?: Array<any>;
-    script?: Array<any>;
-    style?: Array<any>;
-    title?: string;
-    titleAttributes?: Object;
-    titleTemplate?: string;
+  async?: boolean;
+  base?: any;
+  bodyAttributes?: BodyProps;
+  defaultTitle?: string;
+  defer?: boolean;
+  encodeSpecialCharacters?: boolean;
+  htmlAttributes?: HtmlProps;
+  onChangeClientState?: (
+    newState: any,
+    addedTags: HelmetTags,
+    removedTags: HelmetTags,
+  ) => void;
+  link?: LinkProps[];
+  meta?: MetaProps[];
+  noscript?: Array<any>;
+  script?: Array<any>;
+  style?: Array<any>;
+  title?: string;
+  titleAttributes?: Object;
+  titleTemplate?: string;
 }
 
 export class Helmet extends React.Component<HelmetProps> {
-    static peek(): HelmetData;
-    static rewind(): HelmetData;
-    static renderStatic(): HelmetData;
-    static canUseDOM: boolean;
+  static peek(): HelmetData;
+  static rewind(): HelmetData;
+  static renderStatic(): HelmetData;
+  static canUseDOM: boolean;
 }
 
 export interface HelmetData {
-    base: HelmetDatum;
-    bodyAttributes: HelmetHTMLBodyDatum;
-    htmlAttributes: HelmetHTMLElementDatum;
-    link: HelmetDatum;
-    meta: HelmetDatum;
-    noscript: HelmetDatum;
-    script: HelmetDatum;
-    style: HelmetDatum;
-    title: HelmetDatum;
-    titleAttributes: HelmetDatum;
+  base: HelmetDatum;
+  bodyAttributes: HelmetHTMLBodyDatum;
+  htmlAttributes: HelmetHTMLElementDatum;
+  link: HelmetDatum;
+  meta: HelmetDatum;
+  noscript: HelmetDatum;
+  script: HelmetDatum;
+  style: HelmetDatum;
+  title: HelmetDatum;
+  titleAttributes: HelmetDatum;
 }
 
 export interface HelmetDatum {
-    toString(): string;
-    toComponent(): React.Component<any>;
+  toString(): string;
+  toComponent(): React.Component<any>;
 }
 
 export interface HelmetHTMLBodyDatum {
-    toString(): string;
-    toComponent(): React.HTMLAttributes<HTMLBodyElement>;
+  toString(): string;
+  toComponent(): React.HTMLAttributes<HTMLBodyElement>;
 }
 
 export interface HelmetHTMLElementDatum {
-    toString(): string;
-    toComponent(): React.HTMLAttributes<HTMLHtmlElement>;
+  toString(): string;
+  toComponent(): React.HTMLAttributes<HTMLHtmlElement>;
 }
 
 export const peek: () => HelmetData;
